e50ee8586c657bf4d38dc26a21a7187fa05b05ab
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-04-10 23:32:04 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,185 @@
+// Milestone 1
+
+import { useState } from "react";
+
+// const products = [
+//   /*0*/ { name: 'Mela', price: 0.5 },
+//   /*1*/ { name: 'Pane', price: 1.2 },
+//   /*2*/ { name: 'Latte', price: 1.0 },
+//   /*3*/ { name: 'Pasta', price: 0.7 },
+// ];
+//
+// const App = () => {
+//
+//
+//     return (
+//         <ul>
+//             {products.map((product) => <li>{product.name}: {product.price}€</li>)}
+//         </ul>
+//     )
+//
+// }
+//
+// export default App
+//////////////////////////////////////////////////////////////////////////////////////
+
+// Milestone 2
+
+/**
+* 📌 Milestone 2: Aggiungere prodotti al carrello
+Aggiungi uno stato locale addedProducts (inizialmente un array vuoto) per rappresentare i prodotti nel carrello.
+Per ogni prodotto della lista, aggiungi un bottone "Aggiungi al carrello":
+Al click del bottone, usa una funzione addToCart per:
+Aggiungere il prodotto al carrello se non è già presente, con una proprietà quantity = 1.
+Se il prodotto è già nel carrello, ignora l’azione.
+Sotto alla lista dei prodotti, mostra una lista dei prodotti nel carrello se addedProducts contiene almeno un elemento.
+Per ogni prodotto nel carrello, mostra:
+Nome
+Prezzo
+Quantità*/
+
+//Obiettivo: L’utente può aggiungere prodotti al carrello e vedere una lista dei prodotti aggiunti.
+
+// import style from "./style/appStyle.module.css"
+// import { FaCartArrowDown } from "react-icons/fa";
+// 
+// const prodottiInVendita = [
+//     /*0*/ { name: 'Mela', price: 0.5 },
+//     /*1*/ { name: 'Pane', price: 1.2 },
+//     /*2*/ { name: 'Latte', price: 1.0 },
+//     /*3*/ { name: 'Pasta', price: 0.7 },
+// ];
+// 
+// const App = () => {
+// 
+//     const [prodottoAggiunto, setProdottiAggiunto] = useState([])
+// 
+//     function addToCart(prodotto) {
+//         if (!prodottoAggiunto.find((element) => element.name === prodotto.name)) {
+//             prodotto.quantity = 1
+//             setProdottiAggiunto((curr) => [...curr, prodotto])
+//         }
+//     }
+// 
+//     return (
+//         <>
+//             <ul className={`listaProdotti ${style.ul}`}>
+//                 {prodottiInVendita.map((prodotto, index) =>
+//                     <div className={style.d_flex} key={index}>
+//                         <li className={style.li}>{prodotto.name}: {prodotto.price}€</li>
+//                         <button className={style.buttonAdd} onClick={() => { addToCart(prodotto) }}>Aggiungi</button>
+//                         <button className={style.buttonDelete}>Elimina</button>
+//                     </div>
+//                 )}
+//             </ul >
+//             <div className="content">
+//                 <div className={style.flex}>
+//                     <h2>Carrello della Spesa</h2><FaCartArrowDown style={{ fontSize: "30px", marginLeft: "12px" }} />
+//                 </div>
+//                 <div className={style.contenuto}>
+//                     {prodottoAggiunto.map((element, index) =>
+//                         <div key={index}>
+//                             <h4>Name: {element.name}</h4>
+//                             <div className={style.d_flex}>
+//                                 <p>Quantity: <span className={style.quantity}>{element.quantity}</span></p>
+//                                 <p>Price: <span className={style.price}>{element.price}</span></p>
+//                             </div>
+//                         </div>
+//                     )}
+//                 </div>
+//             </div>
+// 
+//         </>
+//     )
+// 
+// }
+// 
+// 
+// 
+// export default App
+
+/**
+📌 Milestone 3: Modificare il carrello
+Al click successivo del bottone "Aggiungi al carrello", se il prodotto è già presente:
+Usa una funzione updateProductQuantity per incrementare la proprietà quantity del prodotto esistente.
+Per ogni prodotto nel carrello, aggiungi un bottone "Rimuovi dal carrello":
+Al click, usa una funzione removeFromCart per rimuovere il prodotto dal carrello.
+Sotto alla lista del carrello, mostra il totale da pagare:
+Calcola il totale moltiplicando il prezzo per la quantità di ogni prodotto e somma tutti i risultati.
+Obiettivo: Gestire l’aggiunta, la rimozione e il calcolo del totale del carrello in modo dinamico.
+*/
+
+
+import style from "./style/appStyle.module.css"
+import { FaCartArrowDown } from "react-icons/fa";
+
+const prodottiInVendita = [
+    /*0*/ { name: 'Mela', price: 0.5 },
+    /*1*/ { name: 'Pane', price: 1.2 },
+    /*2*/ { name: 'Latte', price: 1.0 },
+    /*3*/ { name: 'Pasta', price: 0.7 },
+];
+
+const App = () => {
+
+    const [prodottoAggiunto, setProdottiAggiunto] = useState([])
+
+    function addToCart(prodotto) {
+        if (!prodottoAggiunto.some((element) => element.name === prodotto.name)) {
+            const newProduct = { ...prodotto, quantity: 1 }
+            setProdottiAggiunto((curr) => [...curr, newProduct])
+        }
+    }
+
+    function updateProductQuantity(prodotto) {
+        if (prodottoAggiunto.some((element) => element.name === prodotto.name)) {
+            console.log(prodottoAggiunto)
+            setProdottiAggiunto(prodottoAggiunto.map((element) => element.name === prodotto.name ? { ...element, quantity: element.quantity + 1 } : element))
+        }
+    }
+
+    function removeFromCart() {
+
+    }
+
+    return (
+        <>
+            <ul className={`listaProdotti ${style.ul}`}>
+                {prodottiInVendita.map((prodotto, index) =>
+                    <div className={style.d_flex} key={index}>
+                        <li className={style.li}>{prodotto.name}: {prodotto.price}€</li>
+                        <button className={style.buttonAdd} onClick={() => {
+                            if (!prodottoAggiunto.some((element) => element.name === prodotto.name)) { addToCart(prodotto) }
+                            else if (prodottoAggiunto.some((element) => element.name === prodotto.name)) { updateProductQuantity(prodotto) }
+                        }}>Aggiungi al carrello</button>
+                    </div>
+                )}
+            </ul >
+            <div className="content">
+                <div className={style.flex}>
+                    <h2>Carrello della Spesa</h2><FaCartArrowDown style={{ fontSize: "30px", marginLeft: "12px" }} />
+                </div>
+                <div className={style.contenuto}>
+                    {prodottoAggiunto.map((element, index) =>
+                        <div key={index} className={style.product}>
+                            <h4 className={style.noMargin}>Name: {element.name}</h4>
+                            <div className={`${style.d_flex} ${style.center}`}>
+                                <div className={style.d_flex}>
+                                    <p>Quantity: <span className={style.quantity}>{element.quantity}</span></p>
+                                    <p>Price: <span className={style.price}>{element.price}</span></p>
+                                </div>
+                                <button className={style.buttonDelete} onClick={removeFromCart}>Rimuovi Prodotto</button>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+        </>
+    )
+
+}
+
+
+
+export default App
