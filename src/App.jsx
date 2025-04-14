77c96ee870d9742e9788e0f09f751b4d8f66ac68
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-04-14 16:12:24 +0200

    lasy push

--- src/App.jsx
+++ src/App.jsx
@@ -109,44 +109,56 @@ Calcola il totale moltiplicando il prezzo per la quantità di ogni prodotto e so
 Obiettivo: Gestire l’aggiunta, la rimozione e il calcolo del totale del carrello in modo dinamico.
 */
 
-
-import style from "./style/appStyle.module.css"
+// IMPORTIAMO GLI ELEMENTI NECESSARI ALL'INTERNO DEL NOSTRO FILE JSX
+import style from "./style/appStyle.module.css";
 import { FaCartArrowDown } from "react-icons/fa";
 
+// OGGETTO BASE E COPIA PROFONDA DI QUEST'ULTIMO
 const prodottiInVendita = [
-    /*0*/ { name: 'Mela', price: 0.5 },
-    /*1*/ { name: 'Pane', price: 1.2 },
-    /*2*/ { name: 'Latte', price: 1.0 },
-    /*3*/ { name: 'Pasta', price: 0.7 },
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 }
 ];
+const copiaProdotto = JSON.parse(JSON.stringify(prodottiInVendita));
+console.log(copiaProdotto);
 
 const App = () => {
-
-    const [prodottoAggiunto, setProdottiAggiunto] = useState([])
+    const [prodottoAggiunto, setProdottiAggiunto] = useState([]);
 
     function addToCart(prodotto) {
-        if (!prodottoAggiunto.some((element) => element.name === prodotto.name)) {
-            const newProduct = { ...prodotto, quantity: 1 }
-            setProdottiAggiunto((curr) => [...curr, newProduct])
+        if (!prodottoAggiunto.find((element) => element.name === prodotto.name)) {
+            const newProduct = { ...prodotto, quantity: 1 };
+            console.log(`Questo è il log del nuovo prodotto aggiunto`, newProduct);
+            setProdottiAggiunto((curr) => [...curr, newProduct]);
         }
     }
 
     function updateProductQuantity(prodotto) {
-        if (prodottoAggiunto.some((element) => element.name === prodotto.name)) {
-            console.log(prodottoAggiunto)
-            setProdottiAggiunto(prodottoAggiunto.map((element) => element.name === prodotto.name ? { ...element, quantity: element.quantity + 1 } : element))
+        if (prodottoAggiunto.find((element) => element.name === prodotto.name)) {
+            const constPrice = copiaProdotto.find(element => element.name === prodotto.name).price
+            const updatedProduct = prodottoAggiunto.map((element) => element.name === prodotto.name ? { ...element, quantity: element.quantity + 1, price: element.price + constPrice } : element);
+            console.log(`Questo è il log del prodotto aggiornato`, updatedProduct);
+            setProdottiAggiunto(updatedProduct);
         }
     }
 
-    function removeFromCart() {
-
+    function removeFromCart(prodotto) {
+        const constPrice = copiaProdotto.find(element => element.name === prodotto.name).price
+        console.log(constPrice)
+        if (prodotto.quantity === 1) {
+            const filtredArray = prodottoAggiunto.filter(element => element.name !== prodotto.name);
+            setProdottiAggiunto(filtredArray);
+        } else {
+            setProdottiAggiunto(prodottoAggiunto.map((element) => element.name === prodotto.name ? { ...element, price: element.price - constPrice, quantity: element.quantity - 1 } : element));
+        }
     }
 
     return (
         <>
             <ul className={`listaProdotti ${style.ul}`}>
-                {prodottiInVendita.map((prodotto, index) =>
-                    <div className={style.d_flex} key={index}>
+                {copiaProdotto.map((prodotto, index) =>
+                    <div className={`${style.d_flex} acc`} key={index}>
                         <li className={style.li}>{prodotto.name}: {prodotto.price}€</li>
                         <button className={style.buttonAdd} onClick={() => {
                             if (!prodottoAggiunto.some((element) => element.name === prodotto.name)) { addToCart(prodotto) }
@@ -160,20 +172,24 @@ const App = () => {
                     <h2>Carrello della Spesa</h2><FaCartArrowDown style={{ fontSize: "30px", marginLeft: "12px" }} />
                 </div>
                 <div className={style.contenuto}>
-                    {prodottoAggiunto.map((element, index) =>
+                    {prodottoAggiunto.map((prodotto, index) =>
                         <div key={index} className={style.product}>
-                            <h4 className={style.noMargin}>Name: {element.name}</h4>
+                            <h4 className={style.noMargin}>Name: {prodotto.name}</h4>
                             <div className={`${style.d_flex} ${style.center}`}>
                                 <div className={style.d_flex}>
-                                    <p>Quantity: <span className={style.quantity}>{element.quantity}</span></p>
-                                    <p>Price: <span className={style.price}>{element.price}</span></p>
+                                    <p>Quantity: <span className={style.quantity}>{prodotto.quantity}</span></p>
+                                    <p>Price: <span className={style.price}>{prodotto.price.toFixed(2)}</span></p>
                                 </div>
-                                <button className={style.buttonDelete} onClick={removeFromCart}>Rimuovi Prodotto</button>
+                                <button className={style.buttonDelete} onClick={() => { removeFromCart(prodotto) }}>Rimuovi Prodotto</button>
                             </div>
                         </div>
                     )}
+                    <hr />
+                    <h4>Totale da pagare: {prodottoAggiunto.reduce((acc, curr) => {
+                        return acc + curr.price
+                    }, 0).toFixed(2)}</h4>
                 </div>
-            </div>
+            </div >
 
         </>
     )
